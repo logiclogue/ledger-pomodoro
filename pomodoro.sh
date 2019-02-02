f73aa1f0060f9b4fe36f0b7f2994a10a137167e4
logiclogue <jordanpaullord@hotmail.co.uk>
2019-02-02 11:41:15 +0000

    Almost there

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
 
+flag=
+minutes=25
+seconds=$(( 25 * 60 ))
+account=Untitled
 
+while getopts "t:" flag; do
+    case "${flag}" in
+        t) minutes=$OPTARG;;
+        *) exit 1;;
+    esac
+done
+
+shift $(("${OPTIND}" - 1))
+
+if [ -n "$*" ]; then
+    account="$*"
+fi
+
+start_date=$( date "+%F %T" )
+
+set_start_date () {
+    start_date=$( date "+%F %T" )
+}
+
+write () {
+    echo i "$start_date" "$account"
+    echo o "$( date "+%F %T" )"
+}
+
+while read -r -t "$minutes" new_account; do
+    write
+
+    account=$new_account
+
+    set_start_date
+    set_remaining_seconds
+done
+
+write
